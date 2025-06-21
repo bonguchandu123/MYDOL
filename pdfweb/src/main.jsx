411e6872c5700e815645290104ca12e50db7dd39
bonguchandu123 <bonguchandu7829@gmail.com>
2025-06-21 08:29:14 +0530

    ij

---
+++ pdfweb/src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import {BrowserRouter} from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+import App from './App'
+
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <BrowserRouter>
+        <App />
+        </BrowserRouter>
+      
+    </ClerkProvider>
+
+  
+
+  
+)
