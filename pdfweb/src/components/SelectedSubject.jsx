9f1b1cd41a4a8cb046bc91d332f74e2880094c5b
bonguchandu123 <bonguchandu7829@gmail.com>
2025-06-23 19:53:22 +0530

    subject

--- pdfweb/src/components/SelectedSubject.jsx
+++ pdfweb/src/components/SelectedSubject.jsx
@@ -3,7 +3,33 @@ import { motion, AnimatePresence } from "framer-motion";
 import { ChevronDown, Sparkles } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
-import { subjectsMap } from "../assets/assets";
+// import { subjectsMap } from "../assets/assets";
+ const subjectsMap = {
+  "1": {
+    CSE: ["Calculus and Linear Algebra", "Enginerring Chemistry", "Communicative english", "Basic Electrical And Electronic Engineering","Engineering Drawing","Engineering Chemistry Lab","Communticative English Lab","It Essentials and Python Programming Lab","Probability and Statistics","Applied Physics","Switching Theroy And Logic Design","Programming for Problem solving using C","Enginerring WorkShop","Applied Physics Lab","Programming for Problem Solving using C Lab","Basic Electrical And Electronic Engineering Lab","Environimental Sceince"],
+    CSIT: ["Calculus and Linear Algebra", "Enginerring Chemistry", "Communicative english", "Basic Electrical And Electronic Engineering","Engineering Workshop","Engineering Chemistry Lab","Communticative English Lab","It Essentials Lab","Ordinary differential Equations and Vector Calculus","Applied Physics","Switching Theroy And Logic Design","Programming for Problem solving using C","Enginerring Drawing","Applied Physics Lab","Programming for Problem Solving using C Lab","Basic Electrical And Electronic Engineering Lab","Environimental Sceince"],
+    MECH: ["Calculus and Linear Algebra", "Enginerring Chemistry", "Communicative english", "Basic Electrical And Electronic Engineering","Engineering Workshop","Engineering Chemistry Lab","Communticative English Lab","It Essentials Lab","Ordinary differential Equations and Vector Calculus","Applied Physics","Switching Theroy And Logic Design","Programming for Problem solving using C","Enginerring Drawing","Applied Physics Lab","Programming for Problem Solving using C Lab","Basic Electrical And Electronic Engineering Lab","Environimental Sceince"],
+    CSD:  ["Multivariable calculus", "Enginerring Chemistry", "Communicative english", "Basic Electrical And Electronic Engineering","Engineering Drawing","Engineering Chemistry Lab","Communticative English Lab","It Essentials and Python Programming Lab","Differential Equations and Integral Transforms","Applied Physics","Digital Logic and Compouter design","Programming for Problem solving using C","Enginerring WorkShop","Applied Physics Lab","Programming for Problem Solving using C Lab","Basic Electrical And Electronic Engineering Lab","Environimental Sceince"],
+    CSAI:  ["Multivariable calculus", "Enginerring Chemistry", "Communicative english", "Basic Electrical And Electronic Engineering","Engineering Drawing","Engineering Chemistry Lab","Communticative English Lab","It Essentials and Python Programming Lab","Differential Equations and Integral Transforms","Applied Physics","Digital Logic and Compouter design","Programming for Problem solving using C","Enginerring WorkShop","Applied Physics Lab","Programming for Problem Solving using C Lab","Basic Electrical And Electronic Engineering Lab","Environimental Sceince"],
+    ECE:  ["CalCulus and Linear Algebra", "Enginerring Chemistry", "Communicative english", "Fluid Mechanics and Hydraulic Machines Lab","Engineering Drawing","Engineering Chemistry Lab","Communticative English Lab","Fluid Mechanics and Hydraulic Machines","Ordinary Differential Equation and Vector Calculus","Applied Physics","Elecronic Devices and Circuits","Programming for Problem solving using C","Enginerring WorkShop","Applied Physics Lab","Programming for Problem Solving using C Lab","Electronic Devices and Circuits Lab","Environimental Sceince"],
+    EEE:  ["CalCulus and Linear Algebra", "Enginerring Chemistry", "Communicative english", "Fluid Mechanics and Hydraulic Machines Lab","Engineering Drawing","Engineering Chemistry Lab","Communticative English Lab","Fluid Mechanics and Hydraulic Machines","Ordinary Differential Equation and Vector Calculus","Applied Physics","Elecronic Devices and Circuits","Programming for Problem solving using C","Enginerring WorkShop","Applied Physics Lab","Programming for Problem Solving using C Lab","Electronic Devices and Circuits Lab","Environimental Sceince"],
+    CIVIL: ["CalCulus and Linear Algebra","Chemistry of Materials","Programming for Problem solving using C","Surveying and Geomatics","Enginerring WorkShop","Chemistry Lab","Programming for Problem Solving using C Lab","Surveying and Geomatics Lab","Ordinary differential Equations and Vector Calculus","Engineering Physics","Communticative English","Applied Mechanics","Engineering Drawing","Engineering Physics Lab","Communication English Lab","Engineering Geology Lab","Environimental Sceince"],
+    CHEM: ["CalCulus and Linear Algebra","Physical Chemistry","Programming for Problem solving using C","Basic Electrical and Electronics Engineering","Enginerring WorkShop","Physical Chemistry Lab","Programming for Problem Solving using C Lab","Industrial Chemical Analysis Lab","Ordinary differential Equations and Vector Calculus","Engineering Physics","Communticative English Lab","Applied Mechanics","Engineering Drawing","Engineering Physics Lab","Communication English Lab","Analysis of Organic Compounds Lab","Environimental Sceince"],
+  },
+  "2": {
+    CSE: ["DS", "DBMS", "OOP", "COA"],
+    CSD: ["SE", "DBMS", "OS"],
+    CIVIL: ["BMC", "Surveying"],
+  },
+  "3": {
+    CSE: ["CN", "AI", "TOC"],
+    CSD: ["CN", "OS", "ML"],
+  },
+  "4": {
+    CSE: ["Internship", "Project"],
+  },
+};
+
 
 
 
